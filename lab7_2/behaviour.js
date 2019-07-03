44435de4554b746ad7af70333fd197ea606c00ee
james alituhikya <james.alituhikya@gmail.com>
2019-07-03 21:55:18 +0300

    added lab7 2

--- lab7_2/behaviour.js
+++ lab7_2/behaviour.js
@@ -1,34 +1,34 @@
 (function(){
-"use strict";
-var accountNameText;
-var depositText;
-var createButton;
-var infoTextArea;
-var accountInfoList;
-var makeAccount = (function(){
+    "use strict";
+    var accountInfoList = [];
+    var makeAccount = (function(){
+        return {
+                 createAccount: function(name, amount,infoArea){
+                    if(name && amount){
+                      accountInfoList.push( {
+                                           account: name,
+                                           balance: amount
+                                        });
+                       infoArea.value = "";
+                        for (let i = 0; i < accountInfoList.length; i++) {
+                          infoArea.value +=
+                                          "Account Name: " +
+                                          accountInfoList[i].account +
+                                          " Balance: "+ accountInfoList[i].balance +
+                                          "\n";
+                        }
+                   }
 
-    var accountName =  accountNameText.value;
-    var amount = depositText.value;
-    return {
-             createAccount: function(name, amount){
-             for (let i = 0; i < accountInfoList.length; i++) {
-                infoTextArea.value += accountInfoList[i] + "\n";
-              }
-             }
-           };
-
-    })();
-
-window.onload = function(){
- accountName = document.getElementById("account");
- deposit = document.getElementById("deposit");
- createButton = document.getElementById("create");
- infoTextArea = document.getElementById("info");
- createButton.onclick = function(){
-  accountInfoList.append(makeAccount());
- };
- for (let i = 0; i < accountInfoList.length; i++) {
-   infoTextArea.value += accountInfoList[i] + "\n";
- }
-}
+                 }
+               };
+        })();
+    window.onload = function(){
+     var accountName = document.getElementById("account");
+     var deposit = document.getElementById("deposit");
+     var createButton = document.getElementById("create");
+     var infoTextArea = document.getElementById("info");
+     createButton.onclick = function(){
+          makeAccount.createAccount(accountName.value,deposit.value,infoTextArea);
+     };
+    };
 })();
