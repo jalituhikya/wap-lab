a6f5b1abbedcde968bda5f285c4f73a6b0de679b
james alituhikya <james.alituhikya@gmail.com>
2019-07-09 03:19:03 +0300

    james

--- puzzle/fifteen.js
+++ puzzle/fifteen.js
@@ -1,4 +1,3 @@
-
 $(function() {
     let emptyX = "300px";
     let emptyY = "300px";
@@ -37,26 +36,3 @@ $(function() {
     });
 
  });
-/*    var puzzleArea = document.getElementById('puzzlearea');
-    var divs = puzzleArea.getElementsByTagName("div");
-
-    // initialize each piece
-    for (var i=0; i< divs.length; i++) {
-        var div = divs[i];
-
-        // calculate x and y for this piece
-        var x = ((i % 4) * 100) ;
-        var y = (Math.floor(i / 4) * 100) ;
-
-        // set basic style and background
-        div.className = "puzzlepiece";
-        div.style.left = x + 'px';
-        div.style.top = y + 'px';
-        div.style.backgroundImage = 'url("img/background.jpg")';
-        div.style.backgroundPosition = -x + 'px ' + (-y) + 'px';
-
-        // store x and y for later
-        div.x = x;
-        div.y = y;
-    }
-*/
